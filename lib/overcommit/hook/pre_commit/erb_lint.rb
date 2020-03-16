9d854c545987b7346770f94074b2228c624c5ab0
Tomáš Hruška <hruska@btl.cz>
2020-03-16 18:31:58 +0100

    Add ERB Lint hook

---
+++ lib/overcommit/hook/pre_commit/erb_lint.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Overcommit::Hook::PreCommit
+  # Runs `erb-lint` against any modified Ruby ERB files.
+  #
+  # @see https://github.com/Shopify/erb-lint
+  class ErbLint < Base
+    MESSAGE_REGEX = /^\s*(?<file>(?:\w:)?[^:]+):(?<line>\d+)/
+
+    def run
+      result = execute(command, args: applicable_files)
+      output = result.stdout.chomp.lstrip
+      return :pass if result.success?
+      output = output.split("\n").select { |output| output[/^\s*(?:\w:)?[^:]+:\d+.*/] }
+      # example message:
+      #   path/to/file.rb:1:1: Error message (ruleName)
+      extract_messages(
+          output.grep(MESSAGE_REGEX), # ignore header line
+          MESSAGE_REGEX
+      )
+    end
+  end
+end
